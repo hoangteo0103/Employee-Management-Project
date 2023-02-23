f5ecf126aa487acf45bc622f87d8c36bc03e6d43
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2023-02-23 12:45:43 +0700

    Add some basic validation

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
   AppModule,
   );
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,19 @@
+import { IsEmail , IsNotEmpty } from "class-validator";
 export class CreateUserDto {
+    @IsNotEmpty()  
     username: string;
+    
+    @IsEmail() 
     email: string;
+    
+    @IsNotEmpty()
     password: string;
+    
+    @IsNotEmpty()
     name : string;
+
     contactNumber : string;
+    @IsNotEmpty()
     dateOfBirth : string ;
     refreshToken: string;
     Skills : string[] ;
